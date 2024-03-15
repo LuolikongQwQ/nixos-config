f48819ae0fcac1d146834cdde63d35640c55ef39
LuolikongQwQ <qq7731660@gmail.com>
2024-03-15 22:18:46 +0800

    init: add nixos config by fulanawa add: system init conf

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "simple NixOS flakes";
+  
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    daeuniverse.url = "github:daeuniverse/flake.nix";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.fulanawa-nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./system/config/configuration.nix
+	inputs.daeuniverse.nixosModules.dae
+      ];
+    };
+  };
+}
---
+++ system/config/dae.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.dae = {
+    enable = true;
+    configFile = "/etc/dae/config.dae";
+    assets = with pkgs; [ v2ray-geoip v2ray-domain-list-community ];
+    openFirewall = {
+      enable = true;
+      port = 10000;
+    };
+  };
+}
