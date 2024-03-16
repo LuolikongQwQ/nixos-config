ec883f7537bf61f2b1fb3c09c0c61c2810fe167c
LuolikongQwQ <qq7731660@gmail.com>
2024-03-16 22:52:13 +0800

    fix fcitx5 input

--- desktop/fcitx5.nix
+++ desktop/fcitx5.nix
@@ -4,7 +4,6 @@
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [fcitx5-chinese-addons fcitx5-configtool];
-    fcitx5.plasma6Support = true;
     fcitx5.waylandFrontend = true;
   };
 }
