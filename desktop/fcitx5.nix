f2a73924117aa3b006a3f2b3b9878b0631d5add9
LuolikongQwQ <qq7731660@gmail.com>
2024-03-16 23:02:19 +0800

    fix bugs

--- desktop/fcitx5.nix
+++ desktop/fcitx5.nix
@@ -4,6 +4,6 @@
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [fcitx5-chinese-addons fcitx5-configtool];
-    fcitx5.waylandFrontend = true;
+    #fcitx5.waylandFrontend = true;
   };
 }
