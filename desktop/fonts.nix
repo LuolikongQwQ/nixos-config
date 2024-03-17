1dea41653b1c31c1ae5a1f429d158d0b29e0e7fe
LuolikongQwQ <qq7731660@gmail.com>
2024-03-17 21:47:49 +0800

    updates fonts config

--- desktop/fonts.nix
+++ desktop/fonts.nix
@@ -2,28 +2,43 @@
 
 {
   fonts = {
+    enableDefaultPackages = false;
+    fontDir.enable = true;
+
     packages = with pkgs; [
       # icon fonts
       material-design-icons
+      font-awesome
 
       # normal fonts
       noto-fonts
-      noto-fonts-cjk
-      noto-fonts-emoji
+      noto-fonts-cjk # 汉字部分
+      noto-fonts-emoji # 彩色表情字体
+      #noto-fonts-extra
+      
+      source-sans
+      source-serif
+      source-han-sans
+      source-han-serif
 
       # nerdfonts
-      (nerdfonts.override {fonts = ["FiraCode" "JetBrainsMono"];})
+      (nerdfonts.override {
+        fonts = [
+	  "NerdFontsSymbolsOnly"
+	  "FiraCode"
+	  "JetBrainsMono"
+	];
+      })
+      joulia-mono
+      dejavu_fonts
     ];
-
-    # use fonts specified by user rather than default ones
-    enableDefaultPackages = false;
 
     # user defined fonts
     # the reason there's Noto Color Emoji everywhere is to override DejaVu's
     # B&W emojis that would sometimes show instead of some Color emojis
     fontconfig.defaultFonts = {
-      serif = ["Noto Serif" "Noto Color Emoji"];
-      sansSerif = ["Noto Sans" "Noto Color Emoji"];
+      serif = ["Source Han Serif SC" "Source Han Serif TC" "Noto Color Emoji"];
+      sansSerif = ["Source Han Sans SC" "Source Han Sans TC" "Noto Color Emoji"];
       monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
       emoji = ["Noto Color Emoji"];
     };
