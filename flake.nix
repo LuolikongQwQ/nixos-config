d9ea7521d1bcb1fe1b52ffe3d5e6f9cae835d2a9
LuolikongQwQ <qq7731660@gmail.com>
2024-03-18 16:30:23 +0800

    fix bugs

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, nh, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, nh, ... }@inputs: {
     nixosConfigurations.fulanawa-nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
