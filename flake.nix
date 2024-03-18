2c21c9d80dc80a6dd6495c77cb42f01372522fc9
LuolikongQwQ <qq7731660@gmail.com>
2024-03-18 18:01:55 +0800

    add cache server

--- flake.nix
+++ flake.nix
@@ -48,7 +48,8 @@
             substituters = [
               "https://mirror.sjtu.edu.cn/nix-channels/store"
               "https://cache.nixos.org"
-            ];
+	      "https://nix-community.cachix.org"
+	    ];
             trusted-public-keys = [
               "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             ];
