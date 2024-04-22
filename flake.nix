31631aa465e4798d4e43f23ced74076966367d52
LuolikongQwQ <qq7731660@gmail.com>
2024-04-22 20:43:07 +0800

    fix

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
       system = "x86_64-linux";
       #specialArgs = { inherit inputs; };
       modules = [
-        ./hosts/fulan-home/default.nix
+        ./hosts/fulan0leval/default.nix
 	home-manager.nixosModules.home-manager
 	#inputs.nh.nixosModules.default
 	{
