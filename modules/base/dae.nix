8ce0802b84239731014a2fff10002e5514374fc6
LuolikongQwQ <qq7731660@gmail.com>
2024-05-01 13:52:54 +0800

    fix bugs

--- modules/base/dae.nix
+++ modules/base/dae.nix
@@ -3,7 +3,7 @@
 {
   services.dae = {
     enable = true;
-    package = inputs.daeuniverse.nixosModules.dae;
+    package = inputs.daeuniverse.packages.x86_64-linux.dae;
     configFile = "/etc/dae/config.dae";
     assets = with pkgs; [ v2ray-geoip v2ray-domain-list-community ];
     openFirewall = {
