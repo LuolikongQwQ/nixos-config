d5df3912aadf1e928ab8f15ebba3bd4fef096a5f
LuolikongQwQ <qq7731660@gmail.com>
2024-05-01 13:48:42 +0800

    fix bugs

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
 	  _module.args = { inherit inputs; };
 	  home-manager.useGlobalPkgs = true;
 	  home-manager.useUserPackages = true;
-          home-manager.users.fulanawa = import ./hosts/fulan0level/fulanawa.nix;
+          home-manager.users.fulanawa = import ./hosts/fulan0leval/fulanawa.nix;
           #home-manager.extraSpecialArgs = inputs;
         }
       ];
