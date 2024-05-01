2503f13a9f2ebc1b550290eff83de0338796acbb
LuolikongQwQ <qq7731660@gmail.com>
2024-05-01 13:39:07 +0800

    inputs flakes dae modules

--- modules/base/dae.nix
+++ modules/base/dae.nix
@@ -3,6 +3,7 @@
 {
   services.dae = {
     enable = true;
+    packages = inputs.daeuniverse.nixosModules.dae;
     configFile = "/etc/dae/config.dae";
     assets = with pkgs; [ v2ray-geoip v2ray-domain-list-community ];
     openFirewall = {
