e2fe0e3ccf1c254c318654feede34f4e10807c6a
LuolikongQwQ <qq7731660@gmail.com>
2024-05-15 19:20:37 +0800

    fmt codes

--- hosts/fulan0testing/flake.nix
+++ hosts/fulan0testing/flake.nix
@@ -1,21 +1,20 @@
 {
-  outputs = { std, hive, ... }@inputs:
-  {
+  outputs = { std, hive, ... }@inputs: {
     nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ({ pkgs, ... }: {
-	    users.users.fulanawa = {
-              isNormalUser = true;
-	      initialPassword = "123456";
-            };
-	    environment.systemPackages = with pkgs; [ hello ];
-	    system.stateVersion = "23.11";
-	})
+          users.users.fulanawa = {
+            isNormalUser = true;
+            initialPassword = "123456";
+          };
+          environment.systemPackages = with pkgs; [ hello ];
+          system.stateVersion = "23.11";
+        })
       ];
     };
   };
-  
+
   inputs = {
     nixpkgs-stable.url = "github:nixos/nixpkgs/23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/master";
