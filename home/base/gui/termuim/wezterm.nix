b5b4ce25295f4881a60cd36a1d49ed268994fb21
LuolikongQwQ <qq7731660@gmail.com>
2024-05-20 22:20:00 +0800

    add wezterm pkgs for gui apps

---
+++ home/base/gui/termuim/wezterm.nix
@@ -0,0 +1,53 @@
+{ pkgs, ... }: {
+  programs.wezterm = {
+    enable = true;
+    extraConfig = ''
+      local wezterm = require 'wezterm'
+      local config = {}
+      
+      config.enable_scroll_bar = true
+      
+      config.initial_cols = 100
+      config.initial_raws = 24
+      
+      config.font_size = 16
+      config.font = weztrm.font_with_fallback {
+        'JetBrainsMono Nerd Font Mono',
+	'Source Han Sans HW'
+      }
+      -- TODO: config.color_scheme -> wezterm.colorSchemes
+
+      config.windows_frame = {
+        font = wezterm.font {
+          family = 'JetBrainsMono Mono'
+	},
+	font_size = 14
+      }
+      -- launch menu
+      -- TODO: add ssh
+      -- config.launch_menu = {
+      --   {
+      --      lable = "name",
+      --      args = {
+      --       'wezterm', 'connect', 'name',
+      --     },
+      --   },
+      -- }
+      -- config.ssh_domains = {
+      --   {
+      --     name = "name",
+      --     remote_address = "localhost,127.0.0.1",
+      --     username = "fulanawa",
+      --     multiplexing = "None",
+      --     ssh_option = {
+      --       identityfile = "~/.ssh/fulanawa"
+      --     },
+      --   },
+      -- }
+      config.set_environment_variables = {
+        LANG="C.utf8"
+      }
+      return config
+    '';
+  };
+}
