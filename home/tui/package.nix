dc03a61010b86ba54a75781027b7ccc351b1ade8
LuolikongQwQ <qq7731660@gmail.com>
2024-05-23 19:20:24 +0800

    add 7zip

--- home/tui/package.nix
+++ home/tui/package.nix
@@ -2,18 +2,24 @@
   home.packages = with pkgs; [
     wget
     curl
-    zip
-    unzip
-    xz
+
     ripgrep
     bat
+    
     tree
     pciutils
     file
+    
     nil
     nixfmt
+    
     btop
     nvtop
     iotop
+  
+    zip
+    unzip
+    xz
+    7zip
   ];
 }
