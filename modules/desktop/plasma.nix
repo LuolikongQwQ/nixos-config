42a26ccdd5022898a283b1bb09cff400ceba8527
LuolikongQwQ <qq7731660@gmail.com>
2024-05-24 23:44:22 +0800

    fix bugs

--- modules/desktop/plasma.nix
+++ modules/desktop/plasma.nix
@@ -7,6 +7,6 @@
     qtwebview
   ];
   services.xserver.enable = true;
-  displayManager.sddm.enable = true;
-  desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
 }
