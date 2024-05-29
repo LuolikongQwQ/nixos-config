743c6862b9db9d86486661eebd1565f9e29d1ab0
LuolikongQwQ <qq7731660@gmail.com>
2024-05-29 18:09:25 +0800

    + mpv

--- home/gui/mpv/default.nix
+++ home/gui/mpv/default.nix
@@ -5,10 +5,28 @@
     config = {
       autofit-larger = "x80%";
       autofit-smaller = "1280x720";
+      osd-on-seek = "msg-bar";
       title = "\${?media-title: \${media-title} - }\${mpv-version}";
       force-window-position = false;
       ao = "alsa";
       gpu-api = "opengl";
+
+      alang = "chi,zho,zh,zh-hans,zh-hant,chs,cht,sc,tc,jpn,jp,eng,en";
+      slang = "chi,zho,zh,zh-hans,zh-hant,chs,cht,sc,tc,jpn,jp,eng,en";
+      subs-with-matching-audio = "yes";
+      subs-match-os-language = "no";
+      
+      cache = true;
+      cache-pause-wait = 10;
+      cache-pause-initial = true;
+      demuxer-max-bytes = "128M";
+      demuxer-max-back-bytes = "128M";
+      demuxer-hysteresis-secs = 60;
+      prefetch-playlist = true;
+
+      volume = 100;
+      volume-max = 100;
+
     };
   };
 }
