e64aedadeeaca274983b7fca4f31181837edb239
LuolikongQwQ <qq7731660@gmail.com>
2024-05-29 23:28:19 +0800

    fix bugs

--- modules/desktop/plasma/default.nix
+++ modules/desktop/plasma/default.nix
@@ -4,6 +4,6 @@
     yakuake
     qtwebview
   ];
-  services.diskplayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 }
