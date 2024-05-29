2cbc549b1caa5fed1016edb2293b8b64f5e1ad64
LuolikongQwQ <qq7731660@gmail.com>
2024-05-29 17:59:00 +0800

    ++ mpv

--- home/gui/mpv/default.nix
+++ home/gui/mpv/default.nix
@@ -1,10 +1,12 @@
 { pkgs, ... }: {
   programs.mpv = {
     enable = true;
-    defaultProfiles = [ "gpu-hq" ];
+    defaultProfiles = [ "gpu-hq" "gpu-hq-enhance" ];
     config = {
-      autofit-smaller = "600x100";
-      force-window-position = true;
+      autofit-larger = "x80%";
+      autofit-smaller = "1280x720";
+      title = "\${?media-title: \${media-title} - }\${mpv-version}";
+      force-window-position = false;
       ao = "alsa";
       gpu-api = "opengl";
     };
