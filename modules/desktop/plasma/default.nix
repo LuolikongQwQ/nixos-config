b5b96dcacc9137431269f3de93b8772ecc418438
LuolikongQwQ <qq7731660@gmail.com>
2024-05-29 23:55:35 +0800

    - wayland support

--- modules/desktop/plasma/default.nix
+++ modules/desktop/plasma/default.nix
@@ -4,6 +4,6 @@
     yakuake
     qtwebview
   ];
-  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 }
