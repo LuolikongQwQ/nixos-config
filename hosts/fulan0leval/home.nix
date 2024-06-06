a14cb34b2c0ca74c5a243e585a49e69088b3120b
LuolikongQwQ <qq7731660@gmail.com>
2024-06-06 15:11:41 +0800

    disable ssh-agent

--- hosts/fulan0leval/home.nix
+++ hosts/fulan0leval/home.nix
@@ -9,7 +9,7 @@
   home.username = "fulanawa";
   home.homeDirectory = "/home/fulanawa";
   
-  services.ssh-agent.enable = true;
+  # services.ssh-agent.enable = true;
   home.stateVersion = "23.11";
   programs.home-manager.enable = true;
 }
