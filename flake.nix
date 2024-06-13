7a55a8ad311bdb429ff91d0c336a16b9d6fef1fe
LuolikongQwQ <qq7731660@gmail.com>
2024-06-14 00:17:53 +0800

    inputs hm

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.fulanawa = import ./hosts/fulan0leval/home.nix;
-          #home-manager.extraSpecialArgs = inputs;
+          home-manager.extraSpecialArgs = inputs;
         }
       ];
     };
