3a367d1b7d9eb161d1fb57872c48cfd7c0643598
LuolikongQwQ <qq7731660@gmail.com>
2024-06-18 14:08:15 +0800

    将 daeuniverse inputs 的仓库地址更改为 daeuniverse/flake.nix

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nh, nur-ryan4yin, daeuniverse, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, nh, nur-ryan4yin, daeuniverse-master, ... }@inputs: {
     nixosConfigurations.fulanawa-nixos = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
       modules = [
